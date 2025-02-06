cc32dcec009823e7d81189a560244095136a26da
Zach Khorozian <zkhorozianbc@gmail.com>
2025-02-06 09:59:11 -0500

    sort

--- orderbook/level.py
+++ orderbook/level.py
@@ -6,12 +6,11 @@ from orderbook.order import OrderQueue, Side
 type Price = Decimal
 
 
-@dataclass(order=True)
+@dataclass
 class PriceLevel:
     side: Side = field(compare=False)
     price: Price = field(compare=False)
     orders: OrderQueue = field(default_factory=OrderQueue, compare=False)
-    sort_key: Price = field(compare=True, init=False)
 
-    def __post_init__(self) -> None:
-        self.sort_key = self.price * (-1 if self.side == Side.BUY else 1)
+    def __lt__(self, other: "PriceLevel") -> bool:
+        return self.side.price_comparator(self.price, other.price)
