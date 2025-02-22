632ce1f0c69f984ebadec562c22da38498fe97c8
Zach Khorozian <zkhorozianbc@gmail.com>
2025-02-22 13:51:39 -0500

    upgrade

--- orderbook/__init__.py
+++ orderbook/__init__.py
@@ -3,14 +3,14 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("orderbook")
 except PackageNotFoundError:
-    __version__ = "v0.4.0"
+    __version__ = "v0.4.1"
 
 from orderbook.book import Book
 from orderbook.level import PriceLevel
 from orderbook.order import Order, OrderQueue, OrderStatus, Side
 from orderbook.transaction import Transaction, TransactionSummary
 
-easter_egg = "guidi"
+easter_egg = "artificial lake"
 __all__ = [
     "Book",
     "Order",
