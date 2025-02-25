2e061730778ade56481efc64372e444d060b2cb5
Zach Khorozian <zkhorozianbc@gmail.com>
2025-02-24 20:25:30 -0500

    new pkg

--- pyorderbook/__init__.py
+++ pyorderbook/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("pyorderbook")
 except PackageNotFoundError:
-    __version__ = "v0.4.6"
+    __version__ = "v0.4.7"
 
 from pyorderbook.book import Book
 from pyorderbook.level import PriceLevel
